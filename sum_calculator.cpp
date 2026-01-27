c9482c8bae44e0b0ed62a85877ac0dbed8dadc4d
Cankat23 <cnthgge@gmail.com>
2026-01-27 20:03:29 +0100

    Create sum_calculator.cpp

---
+++ sum_calculator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number = 0;
+    int sum = 0;
+    
+    cout << "Enter number: ";
+    cin >> number;
+    
+    for (int i = 1; i <= number; i++) {
+        
+        sum = sum + i;
+    }
+    cout << "Result: " << sum;
+    
+    return 0;
+}
